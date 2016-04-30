55b410dec57e2a6a03b727b30a7013274355b4c6
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-04-30 19:17:23 +0800

    Update footer

--- visualize/data/footer-data.js
+++ visualize/data/footer-data.js
@@ -2,38 +2,38 @@ var currentDate = new Date()
 
 footer_data = {
   "copyright" : {
-    "year" : currentDate.getFullYear()
+    "year" : currentDate.getFullYear(),
     "organization" : {
-      "name" : "TEELAB"
+      "name" : "TEELAB",
       "link" : "http:\/\/teelab.net\/",
-      "type" : ", Inc."
+      "type" : ", Inc.",
     },
   },
   "powered_by" : [
     {
-      "name" : "GitHub Pages"
+      "name" : "GitHub Pages",
       "link" : "http:\/\/github.io\/",
       "version" : "",
     },
     {
-      "name" : "Bootstrap"
+      "name" : "Bootstrap",
       "link" : "http:\/\/getbootstrap.com\/",
       "version" : "v3.3.6",
     },
     {
-      "name" : "jQuery"
+      "name" : "jQuery",
       "link" : "http:\/\/jquery.com\/",
       "version" : "v1.11.3",
     },
     {
-      "name" : "normalize.css"
+      "name" : "normalize.css",
       "link" : "http:\/\/necolas.github.io\/normalize.css\/",
       "version" : "v4.0.0",
     },
     {
-      "name" : "Font Awesome"
+      "name" : "Font Awesome",
       "link" : "http:\/\/fortawesome.github.io\/Font-Awesome\/",
       "version" : "v4.6.1",
-    }
-  ]
+    },
+  ],
 };
