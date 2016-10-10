8189b60d6a472bad0c9aec122f74bb99a0a2ae8c
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-10-10 15:58:38 +0800

    Update

---
+++ assets/js/set-version.js
@@ -0,0 +1,12 @@
+// set-version.js
+
+var lastCommitTime = '2016-10-03T00:00:00';
+var version = moment(lastCommitTime, 'YYYY-MM-DDTh:mm:ss').utcOffset(8).fromNow();
+
+$('#version')
+    .append(
+        $('<i>')
+        .addClass('fa fa-spinner fa-pulse')
+    )
+    .append(' Updated ')
+    .append(version);
