9f77764c182beff3cc8aaccc504f8cfac5d6253a
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-10-11 11:22:33 +0800

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -1,6 +1,6 @@
 // set-version.js
 
-var lastCommitTime = '2016-10-11T03:47:05';
+var lastCommitTime = '2016-10-11T11:22:05';
 var version = moment(lastCommitTime, 'YYYY-MM-DDTh:mm:ss').utcOffset(8).fromNow();
 
 $('#version')
