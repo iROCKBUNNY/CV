cc107f65a217a256e0caac288a548c3350985011
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-10-27 03:16:10 +0800

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -1,12 +1,10 @@
 // set-version.js
 
-var lastCommitTime = '2016-10-11T11:22:05';
-var version = moment(lastCommitTime, 'YYYY-MM-DDTh:mm:ss').utcOffset(8).fromNow();
-
-$('#version')
-    .append(
-        $('<i>')
-        .addClass('fa fa-refresh fa-spin fa-fw')
-    )
-    .append(' Updated ')
-    .append(version);
+$.getJSON('https://api.github.com/repos/iROCKBUNNY/about-qutong', function(data) {
+    $('#version')
+        .append(
+            $('<i>')
+            .addClass('fa fa-refresh fa-spin fa-fw')
+        )
+        .append(' Updated ' + moment(data.pushed_at, 'YYYY-MM-DDTh:mm:ssZ').fromNow());
+});
