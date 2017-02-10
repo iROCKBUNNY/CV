be3b0584b7a947e477b6dcc9b61e205cf8446ad5
iRB Air <iROCKBUNNY@users.noreply.github.com>
2017-02-10 02:47:43 +0000

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -2,7 +2,7 @@
 
 function updateVersion() {
     $.getJSON('https://api.github.com/repos/iROCKBUNNY/about-qutong', function(data) {
-        $('#version').html('<i class="fa fa-refresh fa-spin fa-fw"></i> Updated ' + moment(data.pushed_at, 'YYYY-MM-DDTh:mm:ssZ').fromNow());
+        $('#version').html('<i class="fa fa-refresh fa-spin fa-fw"></i> Updated ' + moment(data.pushed_at).fromNow());
     });
 };
 updateVersion();
